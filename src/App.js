4e59b98ca510600331c3cd4db831ad02b18c0e8c
ankit <80185690+ankit-dhungana@users.noreply.github.com>
2023-02-19 23:07:10 -0600

    Fetched from API call and updated in DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+//import the images and use it here if needed
+
+import { useEffect, useState } from "react";
+import Descriptions from "./components/Descriptions";
+import { getWeatherData } from "./weatherService";
+
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [units, setUnits] = useState("metric");
+
+  useEffect(() => {
+    const fetchweatherData = async () => {
+      const data = await getWeatherData("Chitawan", units);
+      setWeatherData(data);
+    };
+
+    fetchweatherData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <div className="overlay">
+        {weatherData && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name="city" placeholder="Enter City" />
+              <button>&#176;F</button>
+            </div>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weatherData.name}, ${weatherData.country}`}</h3>
+                <img src={weatherData.iconURL} alt="weather icon" />
+                <h3>{weatherData.description}</h3>
+              </div>
+
+              <div className="temperature">
+                <h1>
+                  {" "}
+                  {`${weatherData.temp.toFixed()}`} &#176;
+                  {units === "metric" ? "C" : "F"}
+                </h1>
+              </div>
+            </div>
+
+            {/* bottom descritpion */}
+            <Descriptions weather={weatherData} units={units}></Descriptions>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/weatherService.js
@@ -0,0 +1,37 @@
+const API_KEY = "15215997ea7e3534345b3fcbf1a6f488";
+
+const getIconURL = (iconId) => `http://openweathermap.org/img/wn/${iconId}.png`;
+
+const getWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: getIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getWeatherData };
